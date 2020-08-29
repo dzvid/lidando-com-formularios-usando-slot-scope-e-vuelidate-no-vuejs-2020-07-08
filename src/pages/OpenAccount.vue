e1b279fa91ca4db23a575516764a6bc228271503
Igor Halfeld <hello@igorluiz.me>
2020-08-29 14:23:53 -0300

    fix for mobile

--- src/pages/OpenAccount.vue
+++ src/pages/OpenAccount.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex items-center w-full h-full bg-brand-black1">
+  <div class="flex items-center w-full pb-16 lg:h-full lg:pb-16 bg-brand-black1">
 
-    <div class="flex justify-center w-full">
-      <div class="flex items-center justify-between w-4/5">
-        <div class="w-1/2 p-16">
+    <div class="flex justify-center w-full mt-24 lg:mt-24">
+      <div class="flex flex-col items-center justify-between w-4/5 lg:flex-row">
+        <div class="w-full p-16 lg:w-1/2">
           <img class="w-full" src="../assets/finance.svg" alt="Ilustração">
         </div>
 
-        <open-account-form class="w-1/2 rounded-sm" />
+        <open-account-form class="w-full rounded-sm lg:w-1/2" />
       </div>
     </div>
 
